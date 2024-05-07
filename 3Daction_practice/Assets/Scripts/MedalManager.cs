b75b00964685e703b19a51a4098a1743cf3f32d8
mikii-f2 <mh.umafate@outlook.jp>
2024-05-08 00:14:00 +0900

    強攻撃の機能追加および物理演算周りの調整

--- 3Daction_practice/Assets/Scripts/MedalManager.cs
+++ 3Daction_practice/Assets/Scripts/MedalManager.cs
@@ -20,7 +20,7 @@ public class MedalManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject.CompareTag("Player")){
+        if (other.gameObject.CompareTag("Player") || other.gameObject.CompareTag("Sword")){
             _animator.SetBool("GetMedal", true);
             StartCoroutine(GetMedal());
         }
--- 3Daction_practice/Assets/Scripts/PlayerManager.cs
+++ 3Daction_practice/Assets/Scripts/PlayerManager.cs
@@ -7,14 +7,13 @@ public class PlayerManager : MonoBehaviour
 {
     private Rigidbody _rb;
     private Transform _transform;
+    private Collider _collider;
     private MeshRenderer _meshRenderer;
-    private Animator _animator;
-    public GameObject effects;
     public int hp = 3;
     private const int horizontalForce = 40;
-    private const int jumpSpeed = 25;
-    private const int continueUpForce = 20;
-    private const int additionalGravity = -40;
+    private const int jumpSpeed = 30;
+    private const int continueUpForce = 30;
+    private const int gravity = -80;
     private const float maxSpeed = 20f;
     private const float jumpTime = 0.2f;
     private float floatingTime = 0;
@@ -40,18 +39,124 @@ public class PlayerManager : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
+        Application.targetFrameRate = 60;           //60FPS固定
         _rb = GetComponent<Rigidbody>();
         _transform = GetComponent<Transform>();
+        _collider = GetComponent<Collider>();
         _meshRenderer = GetComponent<MeshRenderer>();
-        _animator =effects.GetComponent<Animator>();
         playerStatus = Status.DOWN;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //ダメージを受けた直後は操作できない
-        if (!isDamaged)
+        //ダメージを受けた直後、強攻撃直後は操作できない
+        if (!isDamaged && !isAttacked)
+        {
+            //ジャンプ
+            if (Input.GetKeyDown(KeyCode.W) && playerStatus == Status.GROUND)
+            {
+                Vector3 afterJumpVelocity = _rb.velocity;
+                afterJumpVelocity.y = jumpSpeed;
+                _rb.velocity = afterJumpVelocity;
+                isJumped = true;
+            }
+            //2段ジャンプ
+            if (Input.GetKeyDown(KeyCode.W) && (playerStatus == Status.DOWN || floatingTime > 0.15) && isSecondJumpPossible)
+            {
+                Vector3 afterJumpVelocity = _rb.velocity;
+                afterJumpVelocity.y = jumpSpeed;
+                _rb.velocity = afterJumpVelocity;
+                floatingTime = 0;
+                isJumped = true;
+                isSecondJumpPossible = false;
+            }
+        }
+        //空中に出てからの時間を保持
+        if (playerStatus != Status.GROUND)
+        {
+            floatingTime += Time.deltaTime;
+        }
+        //ジャンプ長押し終了
+        if (Input.GetKeyUp(KeyCode.W))
+        {
+            isJumped = false;
+        }
+        //速度プラス検知
+        if (_rb.velocity.y > 1e-4)
+        {
+            playerStatus = Status.UP;
+        }
+        //速度マイナス検知
+        if (_rb.velocity.y < -1e-4)
+        {
+            playerStatus = Status.DOWN;
+        }
+        //進行方向を向く
+        if (playerStatus == Status.GROUND && _rb.rotation != Quaternion.Euler(0f, 0f, 0f))
+        {
+            if (_rb.velocity.x > 0.5f)
+            {
+                _rb.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+            if (_rb.velocity.x < -0.5f && _rb.rotation != Quaternion.Euler(0f, 180f, 0f))
+            {
+                _rb.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+        }
+        //攻撃時や2段ジャンプ時はキー入力によって向きを変える
+        if (Input.GetKeyDown(KeyCode.W) || Input.GetMouseButtonDown(0) || Input.GetMouseButtonDown(1))
+        { 
+            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
+            {
+                _rb.rotation = Quaternion.Euler(0f, 0f, 0f);
+            }
+            if (!Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
+            {
+                _rb.rotation = Quaternion.Euler(0f, 180f, 0f);
+            }
+        }
+        //落下判定
+        if (_transform.position.y < deathLine)
+        {
+            SceneManager.LoadScene("MainScene");
+        }
+        //強攻撃(アニメーションをプレイヤーに直接設定すると挙動がおかしくなるため子オブジェクトのみアニメーションで動かす)
+        if (Input.GetMouseButtonDown(1) && !isAttacked)
+        {
+            isAttacked = true;
+            _collider.enabled = false;
+            StartCoroutine(AttackFinish());
+            _rb.velocity = Vector3.zero;
+            //移動方向はキー入力優先、次点で現在の向き
+            if (Input.GetKey(KeyCode.D))
+            {
+                _rb.position += new Vector3(5, 0, 0);
+            }
+            else if (Input.GetKey(KeyCode.A))
+            {
+                _rb.position += new Vector3(-5, 0, 0);
+            }
+            else if (_transform.eulerAngles.y == 0)
+            {
+                _rb.position += new Vector3(5, 0, 0);
+            }
+            else
+            {
+                _rb.position += new Vector3(-5, 0, 0);
+            }
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        //重力(強攻撃時は空中で静止)
+        if (!isAttacked)
+        {
+            _rb.AddForce(new Vector3(0, gravity, 0));
+        }
+        //ダメージを受けた直後、強攻撃直後は操作できない
+        if (!isDamaged && !isAttacked)
         {
             //右移動
             if (Input.GetKey(KeyCode.D))
@@ -90,80 +195,14 @@ public class PlayerManager : MonoBehaviour
                     }
                 }
             }
-            //ジャンプ
-            if (Input.GetKeyDown(KeyCode.W) && playerStatus == Status.GROUND)
+            //長押しでジャンプ延長
+            if (isJumped)
             {
-                Vector3 afterJumpVelocity = _rb.velocity;
-                afterJumpVelocity.y = jumpSpeed;
-                _rb.velocity = afterJumpVelocity;
-                isJumped = true;
-            }
-            //2段ジャンプ
-            if (Input.GetKeyDown(KeyCode.W) && (playerStatus == Status.DOWN || floatingTime > 0.15) && isSecondJumpPossible)
-            {
-                Vector3 afterJumpVelocity = _rb.velocity;
-                afterJumpVelocity.y = jumpSpeed;
-                _rb.velocity = afterJumpVelocity;
-                floatingTime = 0;
-                isJumped = true;
-                isSecondJumpPossible = false;
-            }
-        }
-        //空中に出てからの時間を保持
-        if (playerStatus != Status.GROUND)
-        {
-            floatingTime += Time.deltaTime;
-        }
-        //長押しでジャンプ延長
-        if (isJumped)
-        {
-            _rb.AddForce(new Vector3(0, continueUpForce, 0));
-            if (floatingTime > jumpTime)
-            {
-                isJumped = false;
-            }
-        }
-        //ジャンプ長押し終了
-        if (Input.GetKeyUp(KeyCode.W))
-        {
-            isJumped = false;
-        }
-        //ジャンプのキレを上げる
-        if (playerStatus != Status.GROUND)
-        {
-            _rb.AddForce(new Vector3(0, additionalGravity, 0));
-        }
-        //速度プラス検知
-        if (_rb.velocity.y > 1e-4)
-        {
-            playerStatus = Status.UP;
-        }
-        //速度マイナス検知
-        if (_rb.velocity.y < -1e-4)
-        {
-            playerStatus = Status.DOWN;
-        }
-        //進行方向を向く
-        if (playerStatus == Status.GROUND){
-            if (_rb.velocity.x > 0.5f)
-            {
-                _transform.eulerAngles = new(0, 0, 0);
-            }
-            if (_rb.velocity.x < -0.5f)
-            {
-                _transform.eulerAngles = new(0, 180, 0);
-            }
-        }
-        //攻撃時や2段ジャンプ時はキー入力によって向きを変える
-        if (Input.GetKeyDown(KeyCode.W) || Input.GetMouseButtonDown(0))
-        { 
-            if (Input.GetKey(KeyCode.D) && !Input.GetKey(KeyCode.A))
-            {
-                _transform.eulerAngles = new(0, 0, 0);
-            }
-            if (!Input.GetKey(KeyCode.D) && Input.GetKey(KeyCode.A))
-            {
-                _transform.eulerAngles = new(0, 180, 0);
+                _rb.AddForce(new Vector3(0, continueUpForce, 0));
+                if (floatingTime > jumpTime)
+                {
+                    isJumped = false;
+                }
             }
         }
         //摩擦の代わり
@@ -185,32 +224,7 @@ public class PlayerManager : MonoBehaviour
                 }
             }
         }
-        //落下判定
-        if (_transform.position.y < deathLine)
-        {
-            SceneManager.LoadScene("MainScene");
-        }
-        //強攻撃(アニメーションをプレイヤーに直接設定すると挙動がおかしくなるため子オブジェクトのみアニメーションで動かす)
-        if (Input.GetMouseButtonDown(1) && !isAttacked)
-        {
-            isAttacked = true;
-            StartCoroutine(AttackFinish());
-            _animator.SetTrigger("Attack2");
-            if (_transform.eulerAngles.y == 0)
-            {
-                _transform.position += new Vector3(5, 0, 0);
-            }
-            else
-            {
-                _transform.position += new Vector3(-5, 0, 0);
-            }
-        }
     }
-
-    //private void FixedUpdate()
-    //{
-        
-    //}
 
     private void OnCollisionStay(Collision collision)
     {
@@ -225,8 +239,8 @@ public class PlayerManager : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        //ダメージ時(ダメージ後の無敵時間を除く)
-        if ((other.CompareTag("Enemy") || other.CompareTag("Bullet")) && !isInvincible)
+        //ダメージ時(ダメージ後の無敵時間および強攻撃中を除く)
+        if ((other.CompareTag("Enemy") || other.CompareTag("Bullet")) && !isInvincible && !isAttacked)
         {
             isDamaged = true;
             isInvincible = true;
@@ -272,5 +286,6 @@ public class PlayerManager : MonoBehaviour
     {
         yield return new WaitForSeconds(attackTime);
         isAttacked = false;
+        _collider.enabled = true;
     }
-    }
+}
